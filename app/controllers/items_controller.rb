ddaec12ffe0157192bfbdced05f2cbca4f948743
rui4210 <69191254+rui4210@users.noreply.github.com>
2020-09-07 15:43:53 +0900

    コードレビュー修正（２回目）
    - itemsコントローラー　@items = Item.allのみ削除する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,10 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
   
+  def index
+    @items = Item.order("created_at DESC")
+  end
+
 
   def new
     @item = Item.new

4c10f6e7f782a9fca54ad023b66e74d45ab73051
rui4210 <69191254+rui4210@users.noreply.github.com>
2020-09-07 15:13:59 +0900

    soldoutのif文の記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :items, only: [:index, :new, :create, :show]
+  resources :item_purchases, only: [:new, :create]
 end

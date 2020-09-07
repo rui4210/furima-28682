c4558812c500f7f68141a978ab2ea83205759f81
rui4210 <69191254+rui4210@users.noreply.github.com>
2020-09-07 15:37:37 +0900

    コードレビュー修正（１回目）
    - itemコントローラー内　一部記述削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
   
-  def index
-    @items = Item.all
-    @items = Item.order("created_at DESC")
-  end
 
   def new
     @item = Item.new

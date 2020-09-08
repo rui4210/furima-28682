32ca68675255f14cb019a5cfd9063c55b5171a2c
rui4210 <69191254+rui4210@users.noreply.github.com>
2020-09-08 12:33:45 +0900

    コードレビュー修正（１回目）
    - itemコントローラー内のdestroyにエラーハンドリング用のif文を追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,8 +33,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to item_path(@item.id)
+    end
   end
 
   private
